426710d03ed02fe74acad1bc6ae686be1915d17a
joaolimareis <victorreislima186@gmail.com>
2025-08-09 22:33:44 -0300

    adinado funcao para exluir itens no array

--- sistemasDeProdutosSimples.js
+++ sistemasDeProdutosSimples.js
@@ -10,11 +10,11 @@ function listarProdutos() {
 }
 
 // Função para procurar um produto pelo nome
-const procurarProduto = (nomeP) => {
-  if (produtos.includes(nomeP)) {
+const procurarProduto = (nome) => {
+  if (produtos.includes(nome)) {
     // const i = produtos.indexOf(nomeP);
     // const nomeProduto = produtos[i];
-    console.log(`Produto "${nomeP}" foi encontrado.`);
+    console.log(`Produto "${nome}" foi encontrado.`);
   } else {
     console.log("Produto não encontrado.");
   }
@@ -26,9 +26,21 @@ const cadastroProduto = (nomeProduto) => {
   console.log(`Produto "${nomeProduto}" adicionado com sucesso.`);
 };
 
+const excluirProduto = (nomeProduto) => {
+  const i = produtos.indexOf(nomeProduto)
+  if(i !== -1){
+    produtos.splice(i,1)
+    console.log(`${nomeProduto} deletado com sucesso`)
+
+  }else{
+    console.log(`Produto ${nomeProduto} nao econtrado`)
+  }
+}
+
 // Executando as funções para teste
 listarProdutos();
 procurarProduto("mouse");
 cadastroProduto("Pia");
+excluirProduto("mouse")
 listarProdutos();
 
