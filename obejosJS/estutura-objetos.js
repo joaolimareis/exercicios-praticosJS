424fdc77e35b184ac1333bc8e65e309c7977c32c
joaolimareis <victorreislima186@gmail.com>
2025-08-19 22:06:51 -0300

    Novo modulo para objetos em JS para Backend

---
+++ obejosJS/estutura-objetos.js
@@ -0,0 +1,10 @@
+const estudante = {
+    nome: 'Jose silva',
+    idade: 16,
+    cpf: '1111111111',
+    Turma: 'AB'
+}
+//O Objeto é sempre composto de conjuntos de chave e valor
+
+console.log(estudante.nome)
+console.log(estutande.cpf.substring(0,3))//metodo para pegar os tres numeros de uma string
